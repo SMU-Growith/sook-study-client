ea3d798751fbe0a25f6b85f1cb147db9eb4ad2e4
khee2 <21keunghee@gmail.com>
2025-09-24 01:26:23 +0900

    chore: prettier 적용

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,14 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
-import reactPlugin from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tailwind from 'eslint-plugin-tailwindcss'
-import prettier from 'eslint-config-prettier'
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tailwind from 'eslint-plugin-tailwindcss';
+import prettier from 'eslint-config-prettier';
 
 export default tseslint.config(
-  { ignores: ['dist', 'node_modules', '.vercel', 'build'] },
+  {ignores: ['dist', 'node_modules', '.vercel', 'build']},
 
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
@@ -22,20 +22,17 @@ export default tseslint.config(
       tailwind,
     },
     extends: [
-      js.configs.recommended,          
-      ...tseslint.configs.recommended, 
-      reactPlugin.configs.flat.recommended, 
-      'plugin:tailwindcss/recommended',     
-      prettier,                        
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      reactPlugin.configs.flat.recommended,
+      'plugin:tailwindcss/recommended',
+      prettier,
     ],
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', {allowConstantExport: true}],
 
-      'tailwindcss/no-custom-classname': 'off', 
+      'tailwindcss/no-custom-classname': 'off',
     },
-  },
-)
+  }
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import "./index.css";
+import './index.css';
 
 function App() {
   return (
     <div className="p-8 space-y-8">
-      <h1 className="font-display text-4xl text-primary-500">
-        HS-Santokki Display Font
-      </h1>
-      <h2 className="text-2xl">
-        Pretendard Variable Sans Font
-      </h2>
+      <h1 className="font-display text-4xl text-primary-500">HS-Santokki Display Font</h1>
+      <h2 className="text-2xl">Pretendard Variable Sans Font</h2>
     </div>
   );
 }
